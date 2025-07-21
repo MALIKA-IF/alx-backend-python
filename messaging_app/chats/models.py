b7628f716fb337f05e9792549c2f6514ccdacb75
Malika Edaif <edmalika957@gmail.com>
2025-07-22 00:26:16 +0100

    add models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 
 # Create your models here.
-class user(AbstractUser):
+class User(AbstractUser):
+       user_id =models.UUIDField(primary_key=True)
+       first_name = models.CharField(max_length=20 ,null= False)
+       last_name = models.CharField(max_length=20 ,null= False)
+       email =models.CharField(max_length=20 , unique=True, null=True)
+       password_hash=models.CharField(max_length=20 , null=False)
        class role(models.TextChoices):
            GUEST ='GUEST','guest'
            HOST= 'HOST','host'
