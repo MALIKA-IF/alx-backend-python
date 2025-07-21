a2a7007e23ba6016d92538dfa6137d09c30cfb4e
Malika Edaif <edmalika957@gmail.com>
2025-07-22 00:17:20 +0100

    add models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class user(AbstractUser):
+       class role(models.TextChoices):
+           GUEST ='GUEST','guest'
+           HOST= 'HOST','host'
+           ADMIN ='ADMIN','admin'
+       phone_number =models.CharField(max_length=20, null=False),
+       role = models.CharField(max_length=20,null=False, choices=role.choices)
+       created_at = models.TimeField(auto_now= True)
+
+
+class conversation(models.Model):
+    conversation_id =models.UUIDField(primary_key=True, unique=True)
+    
+    participants_id = models.ForeignKey(user, on_delete=models.CASCADE)
+    created_at = models.TimeField(auto_now= True)
+
+    
+
+class message(models.Model):
+
+    message_id =models.UUIDField(primary_key=True)
+    sender_id =models.ForeignKey(user,on_delete=models.CASCADE)
+    message_body =models.TextField(null=False)
+    sent_at =models.TimeField(auto_now= True)
+    
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework.permissions.IsAuthenticated',
-            'rest_framework.authentication.SessionAuthentication',
-        ]
-    }
+           'DEFAULT_AUTHENTICATION_CLASSES': [
+           'rest_framework.permissions.IsAuthenticated',
+           'rest_framework.authentication.SessionAuthentication',
+       ]
+   }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'chats.User'
