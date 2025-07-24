f1a1326483c75c0dba88b3b9ecea5e5bf1d038ca
Malika Edaif <edmalika957@gmail.com>
2025-07-24 23:51:11 +0100

    create urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+
+from . import views
+
+urlpatterns =[
+             path("", views.ConversationViewSet, name="conversation"),
+             path("", views.MessageViewSet, name="Message"),
+
+             ]      
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Conversation,Message
 from .serializers import ConversationSerializer,MessageSerializer
+from rest_framework import response
 
 # Create your views here.
 
 class ConversationViewSet(viewsets.ModelViewSet):
+
+    def listConversation(self ,request):
     
-    queryset=Conversation.objects.all()
-    serializer_class = ConversationSerializer
-    data=Conversation.objects.filter().values()
+          queryset=Conversation.objects.all().filter()
+          serializer = ConversationSerializer(queryset, many=True)
+          return  response(serializer.data)
+    
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
+    queryset = Message.objects.all().filter()
     serializer_class = MessageSerializer  
-    data=Message.objects.filter().values()  
+      
